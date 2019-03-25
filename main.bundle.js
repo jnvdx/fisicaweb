09a2dae7344082c8ce8ec0d87376ff575647b888
jnvdx <44271280+jnvdx@users.noreply.github.com>
2019-03-25 07:51:25 +0100

    Add files via upload

---
+++ main.bundle.js
@@ -0,0 +1,315 @@
+webpackJsonp([1,4],{
+
+/***/ 350:
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = 350;
+
+
+/***/ }),
+
+/***/ 351:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__ = __webpack_require__(440);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__environments_environment__ = __webpack_require__(465);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__app_app_module__ = __webpack_require__(461);
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_2__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_3__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=C:/Users/jnvdx/angular/herramienta/src/main.js.map
+
+/***/ }),
+
+/***/ 460:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'app works!';
+    }
+    AppComponent = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["U" /* Component */])({
+            selector: 'app-root',
+            template: __webpack_require__(625),
+            styles: [__webpack_require__(621)]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+//# sourceMappingURL=C:/Users/jnvdx/angular/herramienta/src/app.component.js.map
+
+/***/ }),
+
+/***/ 461:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(195);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__(430);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__(436);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__(460);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__header_header_component__ = __webpack_require__(462);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__mrua_mrua_component__ = __webpack_require__(463);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__tiro_parabolico_tiro_parabolico_component__ = __webpack_require__(464);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */],
+                __WEBPACK_IMPORTED_MODULE_5__header_header_component__["a" /* HeaderComponent */],
+                __WEBPACK_IMPORTED_MODULE_6__mrua_mrua_component__["a" /* MruaComponent */],
+                __WEBPACK_IMPORTED_MODULE_7__tiro_parabolico_tiro_parabolico_component__["a" /* TiroParabolicoComponent */]
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+                __WEBPACK_IMPORTED_MODULE_3__angular_http__["a" /* HttpModule */]
+            ],
+            providers: [],
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppModule);
+    return AppModule;
+}());
+//# sourceMappingURL=C:/Users/jnvdx/angular/herramienta/src/app.module.js.map
+
+/***/ }),
+
+/***/ 462:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return HeaderComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var HeaderComponent = (function () {
+    function HeaderComponent() {
+    }
+    HeaderComponent.prototype.ngOnInit = function () {
+    };
+    HeaderComponent = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["U" /* Component */])({
+            selector: 'app-header',
+            template: __webpack_require__(626),
+            styles: [__webpack_require__(622)]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], HeaderComponent);
+    return HeaderComponent;
+}());
+//# sourceMappingURL=C:/Users/jnvdx/angular/herramienta/src/header.component.js.map
+
+/***/ }),
+
+/***/ 463:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return MruaComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var MruaComponent = (function () {
+    function MruaComponent() {
+    }
+    MruaComponent.prototype.ngOnInit = function () {
+    };
+    MruaComponent = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["U" /* Component */])({
+            selector: 'app-mrua',
+            template: __webpack_require__(627),
+            styles: [__webpack_require__(623)]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], MruaComponent);
+    return MruaComponent;
+}());
+//# sourceMappingURL=C:/Users/jnvdx/angular/herramienta/src/mrua.component.js.map
+
+/***/ }),
+
+/***/ 464:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return TiroParabolicoComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var TiroParabolicoComponent = (function () {
+    function TiroParabolicoComponent() {
+    }
+    TiroParabolicoComponent.prototype.ngOnInit = function () {
+    };
+    TiroParabolicoComponent = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["U" /* Component */])({
+            selector: 'app-tiro-parabolico',
+            template: __webpack_require__(628),
+            styles: [__webpack_require__(624)]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], TiroParabolicoComponent);
+    return TiroParabolicoComponent;
+}());
+//# sourceMappingURL=C:/Users/jnvdx/angular/herramienta/src/tiro-parabolico.component.js.map
+
+/***/ }),
+
+/***/ 465:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=C:/Users/jnvdx/angular/herramienta/src/environment.js.map
+
+/***/ }),
+
+/***/ 621:
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ 622:
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ 623:
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ 624:
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ 625:
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"container\">\r\n  <div class=\"row\">\r\n    <div class=\"col-md-12\">\r\n      <app-header></app-header>\r\n      <app-mrua></app-mrua>\r\n      <app-tiro-parabolico></app-tiro-parabolico>\r\n      <div class=\"card\" style=\"width: 12rem;\">\r\n          <img src=\"http://cosihi.com/wp-content/uploads/images/avatar.png\" class=\"card-img-top\" alt=\"...\">\r\n         <div class=\"card-body\">\r\n           <h5 class=\"card-title\">Jon García</h5>\r\n           <p class=\"card-text\">Hola, soy Jon. Estudiante de 1º de bachiller, apasionado por la informatica y esto es un pequeña aplicacion hecha por mi.</p>\r\n            <a href=\"#\" class=\"btn btn-primary\">Mas informacion sobre mi</a>    \r\n  \r\n        </div>\r\n    </div>\r\n  </div>\r\n</div>"
+
+/***/ }),
+
+/***/ 626:
+/***/ (function(module, exports) {
+
+module.exports = "<nav class=\"navbar navbar-expand-lg navbar-dark bg-primary\">\r\n  <a class=\"navbar-brand\" href=\"#\">Fisica</a>\r\n  <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarNavAltMarkup\" aria-controls=\"navbarNavAltMarkup\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\r\n    <span class=\"navbar-toggler-icon\"></span>\r\n  </button>\r\n  <div class=\"collapse navbar-collapse\" id=\"navbarNavAltMarkup\">\r\n    <div class=\"navbar-nav\">\r\n      <a class=\"nav-item nav-link \" href=\"#\">Home <span class=\"sr-only\">(current)</span></a>\r\n      <a class=\"nav-item nav-link \" href=\"#\">M.R.U.A</a>\r\n      <a class=\"nav-item nav-link \" href=\"#\">Tiro Parabólico</a>\r\n    </div>\r\n  </div>\r\n</nav>"
+
+/***/ }),
+
+/***/ 627:
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"jumbotron\">\r\n  <img src=\"https://www.fisicalab.com/sites/all/files/contenidos/intromov/grafica_x-t_mrua.png\" class=\"img-fluid mx-auto d-block\" alt=\"Responsive image\">\r\n  <p>&nbsp; </p>\r\n  <h1 class=\"display-4\">Calculadora de M.r.u.a</h1>\r\n  <p class=\"lead\">Calculadora muy util para calcular distintos parametros del movimiento rectilineo uniforme</p>\r\n  <hr class=\"my-4\">\r\n  <p>Utilizar solo en caso de no poder resolver un problema</p>\r\n  <hr class=\"my-4\">\r\n  <div class=\"input-group flex-nowrap\">\r\n      <div class=\"input-group-prepend\">\r\n        <span class=\"input-group-text\" id=\"addon-wrapping\">Velocidad inicial</span>\r\n      </div>\r\n      <input type=\"text\" class=\"form-control\" placeholder=\"m/s\" aria-label=\"Username\" aria-describedby=\"addon-wrapping\">\r\n    </div>\r\n    <p>&nbsp; </p>\r\n    <div class=\"input-group flex-nowrap\">\r\n        <div class=\"input-group-prepend\">\r\n          <span class=\"input-group-text\" id=\"addon-wrapping\">Aceleracion</span>\r\n        </div>\r\n        <input type=\"text\" class=\"form-control\" placeholder=\"m/s\" aria-label=\"Username\" aria-describedby=\"addon-wrapping\">\r\n      </div>\r\n    <p>&nbsp; </p>\r\n  <a class=\"btn btn-primary btn-lg\" href=\"#\" role=\"button\">Resolver</a>\r\n  <button type=\"button\" class=\"btn btn-info\">Info</button>\r\n</div>"
+
+/***/ }),
+
+/***/ 628:
+/***/ (function(module, exports) {
+
+module.exports = "\r\n<div class=\"jumbotron\">\r\n  <img src=\"https://www.fisimat.com.mx/wp-content/uploads/2017/11/movimiento_parabolico.png\" class=\"img-fluid mx-auto d-block\" alt=\"Responsive image\">\r\n  <p>&nbsp; </p>\r\n  <h1 class=\"display-4\">Calculadora de Tiro Parabolico</h1>\r\n  <p class=\"lead\">Calculadora muy util para calcular distintos parametros del tiro parabolico</p>\r\n  <hr class=\"my-4\">\r\n  <p>Utilizar solo en caso de no poder resolver un problema</p>\r\n  <hr class=\"my-4\">\r\n  <div class=\"input-group flex-nowrap\">\r\n      <div class=\"input-group-prepend\">\r\n        <span class=\"input-group-text\" id=\"addon-wrapping\">Velocidad inicial</span>\r\n      </div>\r\n      <input type=\"text\" class=\"form-control\" placeholder=\"m/s\" aria-label=\"Username\" aria-describedby=\"addon-wrapping\">\r\n    </div>\r\n    <p>&nbsp; </p>\r\n    <div class=\"input-group flex-nowrap\">\r\n        <div class=\"input-group-prepend\">\r\n          <span class=\"input-group-text\" id=\"addon-wrapping\">Angulo de tiro</span>\r\n        </div>\r\n        <input type=\"text\" class=\"form-control\" placeholder=\"Grados sexagesimales\" aria-label=\"Username\" aria-describedby=\"addon-wrapping\">\r\n      </div>\r\n      <p>&nbsp; </p>\r\n      <div class=\"input-group flex-nowrap\">\r\n          <div class=\"input-group-prepend\">\r\n            <span class=\"input-group-text\" id=\"addon-wrapping\">Altura inicial</span>\r\n          </div>\r\n          <input type=\"text\" class=\"form-control\" placeholder=\"m\" aria-label=\"Username\" aria-describedby=\"addon-wrapping\">\r\n        </div>\r\n    \r\n    <p>&nbsp; </p>\r\n    \r\n  <a class=\"btn btn-primary btn-lg\" href=\"#\" role=\"button\">Resolver</a>\r\n  <button type=\"button\" class=\"btn btn-info\">Info</button>\r\n</div>"
+
+/***/ }),
+
+/***/ 642:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(351);
+
+
+/***/ })
+
+},[642]);
+//# sourceMappingURL=main.bundle.map
